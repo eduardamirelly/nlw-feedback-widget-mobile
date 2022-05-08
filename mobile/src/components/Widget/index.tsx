b8342b958a058961489b249de10cbb437503ce77
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-08 02:05:28 -0300

    :sparkles: feat: navigation into options

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
@@ -16,12 +16,24 @@ import { feedbackTypes } from '../../utils/feedbackTypes';
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   function handleOpen() {
     bottomSheetRef.current?.expand();
   }
-  
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
+  }
+
   return (
     <>
       <TouchableOpacity 
@@ -41,9 +53,27 @@ export function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Form 
-          feedbackType='BUG'
-        />
+        {
+          feedbackSent 
+          ?
+          <Success />
+          :
+          <>
+            {
+              feedbackType 
+              ?
+              <Form 
+                feedbackType={feedbackType}
+                onFeedbackCanceled={handleRestartFeedback}
+                onFeedbackSent={handleFeedbackSent}
+              />
+              :
+              <Options 
+                onFeedbackTypeChanged={setFeedbackType}
+              />
+            }
+          </>
+        }
       </BottomSheet>
     </>
   );
