a414053634f086b592f1e60e5df9ffe83a8c5c72
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-07 21:10:03 -0300

    :heavy_minus_sign: remove expo-app-loading; :heavy_plus_sign: add expo-splash-screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,21 +3,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 
-import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { Widget } from './src/components/Widget';
 
 import { theme } from './src/theme';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
   let [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
