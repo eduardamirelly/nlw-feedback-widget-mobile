b8858574f4d0436631480cd712b3203808cc58d9
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-07 21:02:27 -0300

    :heavy_plus_sign: phosphor package to react-native and expo react-native-svg

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,25 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 
 import AppLoading from 'expo-app-loading';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { Widget } from './src/components/Widget';
 
 import { theme } from './src/theme';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hello World!
-      </Text>
-    </View>
+    <>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+    </>
   );
 }
