6d417837ecc84ceb821aa82de0e6bc0c5cf85d44
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-08 01:29:09 -0300

    :sparkles: feat: styling at Success component is completed

--- mobile/src/components/Success/index.tsx
+++ mobile/src/components/Success/index.tsx
@@ -6,14 +6,30 @@ import {
   TouchableOpacity
 } from 'react-native';
 
-import successImg from '../../assets/successImg.png';
+import successImg from '../../assets/success.png';
+import { Copyright } from '../Copyright';
 
 import { styles } from './styles';
 
 export function Success() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={successImg}
+        style={styles.image}
+      />
 
+      <Text style={styles.title}>
+        Agradecemos o feedback!
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonTitle}>
+          Quero enviar outro
+        </Text>
+      </TouchableOpacity>
+
+      <Copyright />
     </View>
   );
 }
